2ba5334e80c7d3244edb8c86d3165d6f06cc2910
filipeFLemos <filipesflemos@gmail.com>
2019-10-14 20:17:00 +0100

    Contract-net luggage control side implemented

--- src/agents/LuggageAgent.java
+++ src/agents/LuggageAgent.java
@@ -2,6 +2,14 @@ package agents;
 
 import jade.core.AID;
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.lang.acl.UnreadableException;
+import jade.proto.ContractNetResponder;
 import utils.Utils;
 
 public class LuggageAgent extends Agent {
@@ -16,5 +24,60 @@ public class LuggageAgent extends Agent {
     @Override
     protected void setup() {
         System.out.println("Hallo! Luggage-agent "+getAID().getName()+" is ready.");
+        DFAgentDescription dfAgentDescription = new DFAgentDescription();
+        dfAgentDescription.setName(getAID());
+
+        ServiceDescription serviceDescription = new ServiceDescription();
+        serviceDescription.setType("luggage");
+        serviceDescription.setName(getLocalName());
+        dfAgentDescription.addServices(serviceDescription);
+        try {
+            DFService.register(this, dfAgentDescription);
+        } catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+
+        addBehaviour(new TrailSizeAnswerer(this, MessageTemplate.MatchPerformative(ACLMessage.CFP)));
     }
+
+    //Private class responsible for answering queue manager about the available size on its trail
+    class TrailSizeAnswerer extends ContractNetResponder {
+
+        TrailSizeAnswerer(Agent a, MessageTemplate mt) {
+            super(a, mt);
+        }
+
+        @Override
+        protected ACLMessage handleCfp(ACLMessage cfp) {
+            LuggageAgent luggageAgent = (LuggageAgent) myAgent;
+            if (luggageAgent.queueManager == null){
+                luggageAgent.queueManager = cfp.getSender();
+            }
+
+            ACLMessage reply = cfp.createReply();
+            reply.setPerformative(ACLMessage.PROPOSE);
+            reply.setContent("I have space for " + trailFreeSpace + " luggage!");
+
+            return reply;
+        }
+
+        @Override
+        protected void handleRejectProposal(ACLMessage cfp, ACLMessage propose, ACLMessage reject) {
+            //System.out.println(myAgent.getLocalName() + " got a reject...");
+        }
+
+        @Override
+        protected ACLMessage handleAcceptProposal(ACLMessage cfp, ACLMessage propose, ACLMessage accept) {
+            System.out.println(myAgent.getLocalName() + " was selected to pick the next luggage!");
+
+            ACLMessage reply = accept.createReply();
+            reply.setPerformative(ACLMessage.INFORM);
+            reply.setContent("Will be done");
+            //TODO: update trailfreespace
+
+            return reply;
+        }
+
+    }
+
 }
--- src/agents/QueueManagerAgent.java
+++ src/agents/QueueManagerAgent.java
@@ -2,6 +2,10 @@ package agents;
 
 import jade.core.AID;
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
 
 import java.util.Vector;
 
@@ -13,5 +17,42 @@ public class QueueManagerAgent extends Agent {
     public QueueManagerAgent(){
         luggageAgents = new Vector<>();
         peopleScanAgents = new Vector<>();
+    }
+
+    @Override
+    protected void setup() {
+        findAgents();
+    }
+
+    private void findAgents() {
+        DFAgentDescription template = getDFAgentDescriptionTemplate("luggage");
+        try {
+            DFAgentDescription[] result = DFService.search(this, template);
+            System.out.println("Found " + result.length + " Luggage Control Agents.");
+            for (DFAgentDescription agentDescription : result) {
+                luggageAgents.add(agentDescription.getName());
+            }
+        } catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+
+        template = getDFAgentDescriptionTemplate("peopleScan");
+        try {
+            DFAgentDescription[] result = DFService.search(this, template);
+            System.out.println("Found " + result.length + " People Scan Agents.");
+            for (DFAgentDescription agentDescription : result) {
+                peopleScanAgents.add(agentDescription.getName());
+            }
+        } catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+    }
+
+    private DFAgentDescription getDFAgentDescriptionTemplate(String type) {
+        DFAgentDescription template = new DFAgentDescription();
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType(type);
+        template.addServices(sd);
+        return template;
     }
 }
